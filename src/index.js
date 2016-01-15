bf0a03a9b2531b835af9b101d0a10192407cd02b
Eddie Lee <eddie@funktroniclabs.com>
2016-01-14 23:48:26 -0800

    index

---
+++ src/index.js
@@ -0,0 +1,46 @@
+var shell = require("game-shell")()
+
+var context
+  , player_x = 250
+  , player_y = 250
+
+//Bind keyboard commands
+shell.bind("move-left", "left", "A")
+shell.bind("move-right", "right", "D")
+shell.bind("move-up", "up", "W")
+shell.bind("move-down", "down", "S")
+
+//Fired when document is loaded
+shell.on("init", function() {
+  var canvas = document.createElement("canvas")
+  canvas.width = 500
+  canvas.height = 500
+  shell.element.appendChild(canvas)
+  context = canvas.getContext("2d")
+})
+
+//Fired once per game tick
+shell.on("tick", function() {
+  console.log("Tick")
+  if(shell.wasDown("move-left")) {
+    player_x -= 1
+  }
+  if(shell.wasDown("move-right")) {
+    player_x += 1
+  }
+  if(shell.wasDown("move-up")) {
+    player_y -= 1
+  }
+  if(shell.wasDown("move-down")) {
+    player_y += 1
+  }
+})
+
+//Render a frame
+shell.on("render", function(frame_time) {
+  context.fillStyle = "#000"
+  context.fillRect(0, 0, 500, 500)
+
+  context.fillStyle = "#f00"
+  context.fillRect(player_x-10, player_y-10, 20, 20)
+})
