0cbec0ac2691b16292cd45f9a471dabaa759af0c
Eddie Lee <eddie@funktroniclabs.com>
2016-01-15 00:12:29 -0800

    works now!

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,10 +1,28 @@
 var gulp = require('gulp');
-var dirDist = "../dist";
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var browserify = require("browserify");
+var glob = require("glob");
 
-gulp.task('copy', function () {
+var dirDst = "../dist";
+
+gulp.task('html', function () {
   return gulp
-    .src('index.js')
-    .pipe(gulp.dest(dirDist))
-})
+    .src('index.html')
+    .pipe(gulp.dest(dirDst))
+});
+
+gulp.task('browserify', function (cb) {
+  glob('index.js', {}, function (err, files) {
+    var b = browserify();
+    files.forEach(function (file) {
+      b.add(file);
+    });
+    b.bundle()
+      .pipe(source('index.js'))
+      .pipe(gulp.dest(dirDst));
+    cb();
+  }); 
+});
 
 gulp.task('default', ['copy']);
